4f943c719737082f3bbec837f0078507c9893321
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-10-07 01:07:20 +0300

    Task 32 : Add ApiError class

---
+++ src/main/java/com/springbootexceptionhandlingwithaspect/app/exception/error/ApiError.java
@@ -0,0 +1,29 @@
+package com.springbootexceptionhandlingwithaspect.app.exception.error;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class ApiError implements Serializable {
+
+    private int statusCode;
+
+    private HttpStatus status;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    private LocalDateTime timestamp;
+
+    private String message;
+
+    List<String> errorDetails;
+
+}
